bf938a3fb123085ccbdb55f82c74d6eae0903320
Michael Becco <e4e6d4d5@gmail.com>
2015-09-02 13:30:53 -0700

    Add Comment Controller, Modify Post Show
    For Comments, Modify Post Controller for Comments

--- test/features/post/create_post_test.rb
+++ test/features/post/create_post_test.rb
@@ -6,6 +6,7 @@ feature "Post::CreatePost" do
     fill_in 'Title', with: 'This is a brand new title'
     fill_in 'Body', with: 'This is a brand new body'
     click_button 'Create Post'
+    save_and_open_page
     page.must_have_content "This is a brand new title"
     page.must_have_content "This is a brand new body"
   end
