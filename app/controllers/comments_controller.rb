a9cdf0d16e436b3cf8df0ac8166c188f7a5f5992
Michael Becco <e4e6d4d5@gmail.com>
2015-09-02 18:29:18 -0700

    Add First JS Sand Pit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,10 @@ class CommentsController < ApplicationController
     def create
       @post = Post.find(params[:post_id])
       @comment = @post.comments.create(comment_params)
-      redirect_to post_path(@post)
+      respond_to do |format|
+        format.html { redirect_to post_path(@post) }
+        format.js
+      end
     end
 
     private
